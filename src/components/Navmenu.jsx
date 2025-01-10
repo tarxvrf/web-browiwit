82e732fc0a096ea9985045b48eb6d145d5828680
tarxvrf <tarxvrf@gmail.com>
2025-01-10 12:46:35 +0700

    oknav

--- src/components/Navmenu.jsx
+++ src/components/Navmenu.jsx
@@ -2,7 +2,7 @@ import Menu from "@/components/menudropdown/Menu";
 
 export default function Navmenu() {
   return (
-    <div className="fixed w-full top-0 z-50 mx-auto sm:mb-3 outline outline-1 rounded-lg outline-cyan-300 bg-base-300">
+    <div className="fixed top-0 z-50 w-full bg-black border rounded-b-xl">
       <div className="flex justify-between py-3 pr-3"> 
         <div className="px-3 order-1 sm:order-2 ">
           <Menu />
